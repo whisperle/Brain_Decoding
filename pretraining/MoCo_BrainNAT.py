7e02e6fed76b0779d64753ad53122e121704ec70
YXZhang <102130671+ZYXXXXXXXXXX@users.noreply.github.com>
2024-12-11 11:55:17 -0500

    Update MoCo_BrainNAT.py

--- pretraining/MoCo_BrainNAT.py
+++ pretraining/MoCo_BrainNAT.py
@@ -2,21 +2,20 @@
     MoCo v3 architecture for BrainNAT
     Author: Steven Zhang
 """
-import sys
-sys.path.append('/scratch/cc6946/projects/Brain_Decoding/Downstream')
-sys.path.append('/scratch/cc6946/projects/Brain_Decoding')
 import torch
 import torch.nn as nn
 import os
 import torch.distributed as dist
 from backbone import BrainNAT
 
+
 class MoCo_attention(nn.Module):
     """
     Build a MoCo model with a base encoder, a momentum encoder, and two MLPs
     https://arxiv.org/abs/1911.05722
     """
-    def __init__(self, dim= 6724* 64, T=1.0, device='cuda'):
+
+    def __init__(self, dim=192, T=1.0, device='cuda'):
         """
         dim: input feature dimension (see from 1D)
         T: softmax temperature (default: 1.0)
@@ -26,9 +25,9 @@ class MoCo_attention(nn.Module):
         # build encoders & momentum encoder (same structure)
         self.base_encoder = BrainNAT(
             in_chans=1,
-            embed_dim=64,
+            embed_dim=192,
             # pos_embed_dim=pos_embed_dim,
-            depth=9,
+            depth=10,
             num_heads=8,
             num_neighbors=8,
             tome_r=1000,
@@ -36,13 +35,16 @@ class MoCo_attention(nn.Module):
             coord_dim=3,
             omega_0=30,
             last_n_features=16,  # Use the last 16 features for neighborhood attention
-            full_attention=True
+            full_attention=True,
+            progressive_dims=True,
+            initial_tokens=15000,
+            dim_scale_factor=0
         ).to(device)
         self.momentum_encoder = BrainNAT(
             in_chans=1,
-            embed_dim=64,
+            embed_dim=192,
             # pos_embed_dim=pos_embed_dim,
-            depth=9,
+            depth=10,
             num_heads=8,
             num_neighbors=8,
             tome_r=1000,
@@ -50,10 +52,13 @@ class MoCo_attention(nn.Module):
             coord_dim=3,
             omega_0=30,
             last_n_features=16,  # Use the last 16 features for neighborhood attention
-            full_attention=True
+            full_attention=True,
+            progressive_dims=True,
+            initial_tokens=15000,
+            dim_scale_factor=0
         ).to(device)
-        # predictor(MLP), consider not using when backbone output dim is large
-        self.predictor = self._build_mlp(2, dim, 4* 2928, dim, False)
+        # predictor(MLP)
+        self.predictor = self._build_mlp(2, dim, 256, dim, False)
 
         for param_b, param_m in zip(self.base_encoder.parameters(), self.momentum_encoder.parameters()):
             param_m.data.copy_(param_b.data)  # initialize
@@ -80,6 +85,7 @@ class MoCo_attention(nn.Module):
         for param_b, param_m in zip(self.base_encoder.parameters(), self.momentum_encoder.parameters()):
             param_m.data = param_m.data * m + param_b.data * (1. - m)
 
+    # original contrastive_loss
     def contrastive_loss(self, q, k):
         # normalize
         q = nn.functional.normalize(q, dim=1)
@@ -92,7 +98,7 @@ class MoCo_attention(nn.Module):
         labels = (torch.arange(N, dtype=torch.long) + N * torch.distributed.get_rank()).cuda()
         return nn.CrossEntropyLoss()(logits, labels) * (2 * self.T)
 
-    def forward(self, v1, c1, v2, c2, m):
+    def forward(self, v, c, m, jepa_mask=False, mask_indices=None, mask_token=None):
         """
         Input:
             v1,c1: first views of data
@@ -102,25 +108,26 @@ class MoCo_attention(nn.Module):
             loss
         """
         # query encoder
-        q1 = self.base_encoder(v1, c1)
-        q2 = self.base_encoder(v2, c2)
-        dim0, dim1, dim2 = q1.shape
-        q1 = q1.view(dim0, dim1 * dim2)
-        q2 = q2.view(dim0, dim1 * dim2)
-        # print("q1 shape"+ str(q1.shape))
+        q1 = self.base_encoder(v, c, jepa_mask, mask_indices, mask_token)
+        q2 = self.base_encoder(v, c, jepa_mask, mask_indices, mask_token)
+        # print(q1.shape)
         # q & k shape: [b, 6724, 64]
 
-        # compute features through mlp layers (deleted in this version because backbone's output is too large)
-        # q1 = self.predictor(q1)
-        # q2 = self.predictor(q2)
+        # do the average
+        q1 = torch.mean(q1, dim=1)
+        q2 = torch.mean(q2, dim=1)
+
+        q1 = self.predictor(q1)
+        q2 = self.predictor(q2)
 
         with torch.no_grad():  # no gradient
             self._update_momentum_encoder(m)  # update the momentum encoder
             # compute momentum features as targets
-            k1 = self.momentum_encoder(v1, c1)
-            k2 = self.momentum_encoder(v2, c2)
-            k1 = k1.view(dim0, dim1 * dim2)
-            k2 = k2.view(dim0, dim1 * dim2)
+            k1 = self.momentum_encoder(v, c, jepa_mask, mask_indices, mask_token)
+            k2 = self.momentum_encoder(v, c, jepa_mask, mask_indices, mask_token)
+            # also do average
+            k1 = torch.mean(k1, dim=1)
+            k2 = torch.mean(k2, dim=1)
         return self.contrastive_loss(q1, k2) + self.contrastive_loss(q2, k1)
 
 
@@ -139,19 +146,20 @@ def concat_all_gather(tensor):
 
 
 if __name__ == '__main__':
-    # initialize the default process group
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-    dist.init_process_group(backend='gloo', rank=0, world_size=1)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    torch.distributed.init_process_group(backend="nccl")
 
-    model = MoCo_attention()
+    model = MoCo_attention().to(device)
     model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
     model.cuda()
+    model.train()
+    v1 = torch.randn(9, 1, 15724, device=device)
+    c1 = torch.randn(9, 15724, 3, device=device)
 
-    v1 = torch.randn(9, 1, 15724).cuda()
-    c1 = torch.randn(9, 15724, 3).cuda()
-
-    v2 = torch.randn(9, 1, 15724).cuda()
-    c2 = torch.randn(9, 15724, 3).cuda()
+    v2 = torch.randn(9, 1, 15724, device=device)
+    c2 = torch.randn(9, 15724, 3, device=device)
     loss = model(v1, c1, v2, c2, 1)
     print(loss)
+    loss.backward()
+
+    torch.distributed.destroy_process_group()
