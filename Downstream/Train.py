fix bug in multi gpu gather no gradient

Downstream/Train.py
@@ -468,7 +468,7 @@ def train(args, model, diffusion_prior, train_dl, test_dl, accelerator, data_typ
         disable=not accelerator.is_local_main_process
     )
     
-    global_iteration = 0
+    global_iteration = epoch_start * num_iterations_per_epoch
     for epoch in epoch_progress:
         model.train()
         iteration = 0
@@ -626,13 +626,13 @@ def train(args, model, diffusion_prior, train_dl, test_dl, accelerator, data_typ
                 if accelerator.is_main_process and wandb_log:
                     wandb.log({
                         "train/loss_per_iter": loss.item(),
-                        "train/blurry_pixcorr_per_iter": blurry_pixcorr_per_iter / args.batch_size,
-                        "train/recon_cossim_per_iter": recon_cossim_per_iter / args.batch_size,
-                        "train/recon_mse_per_iter": recon_mse_per_iter / args.batch_size,
-                        "train/loss_prior_per_iter": loss_prior_per_iter / args.batch_size,
-                        "train/loss_clip_per_iter": loss_clip_per_iter / args.batch_size,
-                        "train/loss_blurry_per_iter": loss_blurry_per_iter / args.batch_size,
-                        "train/loss_blurry_cont_per_iter": loss_blurry_cont_per_iter / args.batch_size,
+                        "train/blurry_pixcorr_per_iter": blurry_pixcorr_per_iter,
+                        "train/recon_cossim_per_iter": recon_cossim_per_iter,
+                        "train/recon_mse_per_iter": recon_mse_per_iter,
+                        "train/loss_prior_per_iter": loss_prior_per_iter,
+                        "train/loss_clip_per_iter": loss_clip_per_iter,
+                        "train/loss_blurry_per_iter": loss_blurry_per_iter,
+                        "train/loss_blurry_cont_per_iter": loss_blurry_cont_per_iter,
                     }, step=global_iteration)
 
                 iteration += 1
@@ -714,13 +714,13 @@ def train(args, model, diffusion_prior, train_dl, test_dl, accelerator, data_typ
                 random_samps = np.random.choice(np.arange(len(image)), size=len(image)//5, replace=False)
                 
                 if use_prior:
-                    diffusion_prior(text_embed=backbone[random_samps], image_embed=clip_target[random_samps])
+                    loss_prior, prior_out = diffusion_prior(text_embed=backbone[random_samps], image_embed=clip_target[random_samps])
                     test_loss_prior_total += loss_prior.item()
                     loss_prior *= prior_scale
                     loss += loss_prior
                     # TODO: this two line was not tested
-                    test_recon_cossim += nn.functional.cosine_similarity(prior_out, clip_target).mean().item()
-                    test_recon_mse += mse(prior_out, clip_target).item()
+                    test_recon_cossim += nn.functional.cosine_similarity(prior_out, clip_target[random_samps]).mean().item()
+                    test_recon_mse += mse(prior_out, clip_target[random_samps]).item()
                 
                 if clip_scale>0:
                     loss_clip = utils.soft_clip_loss(

Downstream/utils.py
@@ -134,8 +134,8 @@ def gather_features(image_features, voxel_features, accelerator):
 def soft_clip_loss(preds, targs, accelerator=None, temp=0.125):
     # Gather tensors across all GPUs if using distributed training
     if accelerator is not None and accelerator.num_processes > 1:
-        preds = accelerator.gather(preds)
-        targs = accelerator.gather(targs)
+        preds = torch.cat(torch.distributed.nn.all_gather(preds), dim=0)
+        targs = torch.cat(torch.distributed.nn.all_gather(targs), dim=0)
     
     clip_clip = (targs @ targs.T) / temp
     brain_clip = (preds @ targs.T) / temp
@@ -148,11 +148,7 @@ def soft_clip_loss(preds, targs, accelerator=None, temp=0.125):
     
     # Scale the loss to avoid over-counting gradients.
     # By default, each GPU now sees the entire global batch and calls backward.
-    # To correct for this, divide by the number of processes.
-    if accelerator is not None and accelerator.num_processes > 1:
-        world_size = accelerator.num_processes
-        loss = loss / world_size
-    
+    # To correct for this, divide by the number of processes.    
     return loss
 
 def soft_siglip_loss(preds, targs, temp, bias):
@@ -209,8 +205,8 @@ def mixco_nce(preds, targs, temp=0.1, perm=None, betas=None, select=None, distri
               accelerator: Accelerator =None, local_rank=None, bidirectional=True):
 
     if accelerator is not None and accelerator.num_processes > 1:
-        preds = accelerator.gather(preds)
-        targs = accelerator.gather(targs)
+        preds = torch.cat(torch.distributed.nn.all_gather(preds), dim=0)
+        targs = torch.cat(torch.distributed.nn.all_gather(targs), dim=0)
         if betas is not None:
             betas = accelerator.gather(betas)
         if perm is not None:
@@ -233,10 +229,6 @@ def mixco_nce(preds, targs, temp=0.1, perm=None, betas=None, select=None, distri
         if bidirectional:
             loss2 = F.cross_entropy(brain_clip.T, torch.arange(brain_clip.shape[0]).to(brain_clip.device))
             loss = (loss + loss2)/2
-    
-    if accelerator is not None and accelerator.num_processes > 1:
-        world_size = accelerator.num_processes
-        loss = loss / world_size
     
     return loss
     